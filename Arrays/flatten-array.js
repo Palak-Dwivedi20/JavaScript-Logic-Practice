b21e25b2f97ae6dec53514056f33752ba648677f
Palak Dwivedi <dwivedipalak23@gmail.com>
2026-05-30 23:21:53 +0530

    Added flatten array logic question

---
+++ Arrays/flatten-array.js
@@ -0,0 +1,20 @@
+// Flatten a nested array into a single array.
+
+function flattenArray(arr) {
+    let result = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            for(let j = 0; j < arr[i].length; j++) {
+                result.push(arr[i][j])
+            }
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+
+console.log(flattenArray([1, 2, [3, 4], 5]));
