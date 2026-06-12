fbd6db452b44645cb05e2120469ed900b8bc70af
Palak Dwivedi <dwivedipalak23@gmail.com>
2026-06-12 23:33:41 +0530

    Solved Maximum Consecutive Ones

---
+++ Arrays/max-consecutive-ones.js
@@ -0,0 +1,20 @@
+// Find Maximum Consecutive Ones
+
+function maxConsecutiveOnes(arr) {
+    let count = 0;
+    let maxCount = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === 1) {
+            count++;
+            maxCount = Math.max(maxCount, count)
+        }
+        else {
+            count = 0;
+        }
+    }
+
+    return maxCount;
+}
+
+console.log(maxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
