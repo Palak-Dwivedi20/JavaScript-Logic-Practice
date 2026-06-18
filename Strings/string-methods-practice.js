e48af3a2c465a57995d0c35bfe5b330622c8981a
Palak Dwivedi <dwivedipalak23@gmail.com>
2026-06-18 23:27:11 +0530

    Practice string methods in JavaScript

---
+++ Strings/string-methods-practice.js
@@ -0,0 +1,33 @@
+// String Methods Practice ___
+
+//Check if a String Contains Another String in JavaScript?
+let str = "Hello World".toLowerCase();
+
+console.log(str.includes("Orl".toLowerCase()));
+
+
+//   Check if String Starts With
+//  Check if String ends With
+
+let str2 = "JavaScript";
+
+console.log(str2.toLowerCase().startsWith("java"));
+console.log(str2.toLowerCase().endsWith("script"));
+
+
+// Check length of a string
+let str3 = "JavaScript";
+
+console.log(str3.length);
+
+
+// Get Character at Specific Index
+let str4 = "JavaScript";
+
+console.log(str4.charAt(4));
+
+
+//Find Index of Character/String
+let str5 = "JavaScript";
+
+console.log(str5.indexOf("S"));
